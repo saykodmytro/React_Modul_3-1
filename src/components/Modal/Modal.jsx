7438129adc0ea55dfbe5bf4b72688a1138abd151
Dmytro Sayko <saykodmytro@icloud.com>
2023-10-25 15:18:37 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,14 +2,22 @@ import css from './Modal.module.css';
 import React, { Component } from 'react';
 
 export default class Modal extends Component {
-  // componentDidMount() {
-  //   window.addEventListener('keydown', this.handleKeyDown);
-  //   document.body.style.overflow = 'hidden';
-  // }
+  state = {
+    counter: 1,
+  };
 
-  // componentWillUnmount() {
-  //   window.removeEventListener('keydown', this.handleKeyDown);
-  // }
+  handleIncrementProduct = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 1 }));
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+    document.body.style.overflow = 'hidden';
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
 
   handleOverlayClick = evt => {
     if (evt.target === evt.currentTarget) {
@@ -36,7 +44,9 @@ export default class Modal extends Component {
             <p>Discount: {this.props.modalData.discount}$</p>
             <p>Price: {this.props.modalData.price}$</p>
           </div>
-          <button type="submit">Add Products</button>
+          <button onClick={this.handleIncrementProduct}>
+            Add Product: {this.state.counter}
+          </button>
         </div>
       </div>
     );
